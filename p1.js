bca70f6beb83e09a53d5a988076f7260f4057fd5
SatoNekone <satonekora@gmail.com>
2017-03-06 10:53:25 +0800

    APP_hw1

---
+++ p1.js
@@ -0,0 +1,7 @@
+// Your Javascript Code Goes Here
+var name = "hahahaha";
+var age = 18;
+
+console.log("My name is " + name + ", and My age is " + age);
+
+console.log(`My name is ${name}, and My age is ${age}`);
